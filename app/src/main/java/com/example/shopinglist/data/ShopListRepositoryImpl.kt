7a123c28937b063477eff9a058c0cc6c6aa7d2d5
vlad u. <itvalidol@gmail.com>
2024-02-20 17:52:56 +0300

    Linearlayout

--- app/src/main/java/com/example/shopinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/example/shopinglist/data/ShopListRepositoryImpl.kt
@@ -8,17 +8,19 @@ import java.lang.RuntimeException
 
 object ShopListRepositoryImpl : ShopListRepository {
     private val shopListLD = MutableLiveData<List<ShopItem>>()
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({ o1, o2 -> o1.id.compareTo(o2.id) })
 
     private var autoIncrementId = 0
+
     init {
-        for (i in 0 until 10){
+        for (i in 0 until 10) {
             val item = ShopItem("Name $i", i, true)
             addShopItem(item)
         }
     }
+
     override fun addShopItem(shopItem: ShopItem) {
-        if (shopItem.id == ShopItem.UNDEFINED_ID){
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
             shopItem.id = autoIncrementId++
         }
         shopList.add(shopItem)
@@ -44,7 +46,8 @@ object ShopListRepositoryImpl : ShopListRepository {
     override fun getShopList(): LiveData<List<ShopItem>> {
         return shopListLD
     }
-    private fun updateList(){
+
+    private fun updateList() {//каждый раз когда что-то удаляется/добавляется, то мы устанавливаем все в shopListLD
         shopListLD.value = shopList.toList()
     }
 }
--- app/src/main/java/com/example/shopinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shopinglist/presentation/MainActivity.kt
@@ -3,24 +3,45 @@ package com.example.shopinglist.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import com.example.shopinglist.R
+import com.example.shopinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
-    private var count = 0
+    private lateinit var llShopItem: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        llShopItem = findViewById(R.id.ll_shop_list)
         viewModel=ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this){
-            Log.d("MainActivity",it.toString())
-            if(count==0){
-                count++
-                val item = it[0]
-                viewModel.changeEnableState(item)
-            }
+            showList(it)
 
+        }
+    }
+    private fun showList(list: List<ShopItem>){
+        llShopItem.removeAllViews()//вызывается каждый раз при изменение обсервера
+        for (shopItem in list){
+            val layoutId = if (shopItem.enabled){
+                R.layout.item_shop_enabled
+            }else{
+                R.layout.item_shop_disabled
+            }
+            val view = LayoutInflater.from(this).inflate(layoutId,llShopItem,false)
+            val tvName = view.findViewById<TextView>(R.id.tv_name)
+            val tvCount = view.findViewById<TextView>(R.id.tv_count)
+            tvName.text = shopItem.name
+            tvCount.text = shopItem.count.toString()
+            view.setOnLongClickListener(){
+                viewModel.changeEnableState(shopItem)
+                true
+            }
+            llShopItem.addView(view)// вызывается каждый раз и грузит систему
         }
     }
 }
