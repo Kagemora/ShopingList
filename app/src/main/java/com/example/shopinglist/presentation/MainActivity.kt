a41bbaaa06599247479047183c934ecb54073357
vlad u. <itvalidol@gmail.com>
2024-02-26 23:47:04 +0300

    final version

--- app/src/main/java/com/example/shopinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shopinglist/presentation/MainActivity.kt
@@ -3,20 +3,28 @@ package com.example.shopinglist.presentation
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainer
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.FragmentHostCallback
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shopinglist.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import javax.net.ssl.HostnameVerifier
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ShopItemFragment.OnEditingFinishedListener {
     private lateinit var viewModel: MainViewModel
     private lateinit var adapter: ShopListAdapter
+    private var shopItemContainer: FragmentContainerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        shopItemContainer = findViewById(R.id.shop_item_container)
         setupRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
@@ -24,10 +32,31 @@ class MainActivity : AppCompatActivity() {
             //если нужно обновить список вызываем submitList
         }
         val buttonAddItem = findViewById<FloatingActionButton>(R.id.button_add_shop_item)
-        buttonAddItem.setOnClickListener{
-            val intent = ShopItemActivity.newIntentAddItem(this)
-            startActivity(intent)
+        buttonAddItem.setOnClickListener {
+            if (isOnePaneMode()) {
+                val intent = ShopItemActivity.newIntentAddItem(this)
+                startActivity(intent)
+            } else {
+                lauchFragment(ShopItemFragment.newInstanceAddItem())
+            }
         }
+    }
+
+    override fun onEditingFinished() {
+        Toast.makeText(this@MainActivity, "Succes", Toast.LENGTH_SHORT).show()
+        supportFragmentManager.popBackStack()
+    }
+
+    private fun isOnePaneMode(): Boolean {
+        return shopItemContainer == null
+    }
+
+    private fun lauchFragment(fragment: Fragment) {
+        supportFragmentManager.popBackStack()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.shop_item_container, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 
     private fun setupRecyclerView() {
@@ -50,9 +79,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupClickListener() {
         adapter.onShopItemClickListener = {
-            Log.d("MainActivity", it.toString())
-            val intent = ShopItemActivity.newIntentEditItem(this,it.id)
-            startActivity(intent)
+            if (isOnePaneMode()) {
+                val intent = ShopItemActivity.newIntentEditItem(this, it.id)
+                startActivity(intent)
+            } else {
+                lauchFragment(ShopItemFragment.newInstanceEditItem(it.id))
+            }
+
         }
     }
 
@@ -60,7 +93,8 @@ class MainActivity : AppCompatActivity() {
         val callbakc = object : ItemTouchHelper.SimpleCallback(
             0,
             ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
-        ) {//параметр 0 говорит, что мы не поддерживаем перетаскивание элементво списка
+        ) {
+            //параметр 0 говорит, что мы не поддерживаем перетаскивание элементво списка
             override fun onMove(
                 recyclerView: RecyclerView,
                 viewHolder: RecyclerView.ViewHolder,
@@ -77,7 +111,8 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
-        val itemTouchHelper = ItemTouchHelper(callbakc)//Создание экземпляра ItemTouchHelper, передавая в качестве параметра ранее созданный callback.
+        val itemTouchHelper =
+            ItemTouchHelper(callbakc)//Создание экземпляра ItemTouchHelper, передавая в качестве параметра ранее созданный callback.
         itemTouchHelper.attachToRecyclerView(rvShopList)//Привязка ItemTouchHelper к RecyclerView, таким образом, активируя функциональность свайпа для элементов списка.
         //передали в attachToRecyclerView как должен работать спайп
     }
